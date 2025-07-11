7dbe2f91192ff6673938a6a36f0989518b90dbac
pushkar <pushkar.agarwal@audviklabs.com>
2025-07-11 16:52:02 +0530

    debug the timezone, moved from datetime to django.utils

--- wallets/views.py
+++ wallets/views.py
@@ -1,4 +1,5 @@
-from datetime import timedelta, timezone
+from datetime import timedelta
+from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
