aa297ed5346ef053bb6a3821cf1822dde26cc1d7
pushkar <pushkar.agarwal@audviklabs.com>
2025-07-11 12:29:11 +0530

    added rate limiting to block brute force

--- secure_wallet_api/settings.py
+++ secure_wallet_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 from decouple import config
 
@@ -42,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'users'
+    'users',
+    'wallets'
 ]
 
 MIDDLEWARE = [
@@ -71,6 +73,17 @@ TEMPLATES = [
         },
     },
 ]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'ALGORITHM': 'HS256',  # For now. Move to RS256 (asymmetric) in prod.
+}
 
 WSGI_APPLICATION = 'secure_wallet_api.wsgi.application'
 
